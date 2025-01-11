0851486c4d3506ccfaca35a614c412ecf2a8c22f
Luciano Wandame <luwandame@gmail.com>
2025-01-11 16:59:55 -0300

    first commit

--- ex02.py
+++
@@ -1,8 +0,0 @@
-#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
-
-numero = float(input("Digite um número: "))
-
-if numero > 0:
-    print("O número digitado é positivo")
-else:
-    print("O número digitado é negativo")
--- ex04.py
+++
@@ -1,7 +0,0 @@
-#Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
-letraDigitada = input("Digite uma letra: ")
-
-if letraDigitada == "a" or letraDigitada == "e" or letraDigitada == "i" or letraDigitada == "o" or letraDigitada == "u":
-    print("Vogal")
-else:    
-    print("Consoante")
